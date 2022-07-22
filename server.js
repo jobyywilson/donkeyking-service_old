342c137a08870f73bbc300d632f5d6dab8c37882
Joby Wilson Mathews <35163814+jobyywilson@users.noreply.github.com>
2022-07-22 13:07:54 +0530

    corrected cards

--- server.js
+++ server.js
@@ -26,8 +26,7 @@ io.on('connection', socket => {
     })
   })
   socket.on('start-game', (roomId,userIds) => {
-    let cards=["AC","2C","3C","4C","5C","6C","7C","8C","9C","JC","QC","KC","AD","2D","3D","4D","5D","6D","7D","8D","9D","JD","QD","KD","AH","AH","2H","3H","4H","5H","6H","7H","8H","9H","JH","QH","KH","AS","AS","2S","3S","4S","5S","6S","7S","8S","9S","JS","QS","KS"]
-    //let cards=["AC","2C","3S","AD","2D","3H"]
+    let cards=["AC","AD","AH","AS","2C","2D","2H","2S","3C","3D","3H","3S","4C","4D","4H","4S","5C","5D","5H","5S","6C","6D","6H","6S","7C","7D","7H","7S","8C","8D","8H","8S","9C","9D","9H","9S","10C","10D","10H","10S","JC","JD","JH","JS","QC","QD","QH","QS","KC","KD","KH","KS"]    //let cards=["AC","2C","3S","AD","2D","3H"]
 
     let shuffledCards = cards.sort(function(){return 0.5-Math.random()})
     let clients_in_the_room = io.sockets.adapter.rooms[roomId].sockets; 
